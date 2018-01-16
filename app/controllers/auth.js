0e3cca951345d2ddf2f28f3c98d0bbbc3c77d61f
Mehdi Druon <mehdidruon@gmail.com>
2018-01-16 12:29:54 +0100

    Add check password & email in auth.

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,22 +1,28 @@
 const express = require("express");
 const model = require("../models/user");
+const { encode } = require('./pwd');
 
 const router = express.Router();
 
 router.post("/login", (req, res) => {
   const { lastname, firstname, email, password } = req.body;
-  model
-    .createUser({ firstname, lastname, email, password, user: "user" })
-    .then(result => res.send(result))
-    .catch(err => console.log(err));
+  userInfo.notExist(email)
+  .then(result => encode(password))
+  .then(hash =>
+    user.createUser({ firstname, lastname, email, hash, role: "user" })
+   )
+   .then(result => res.send(result))
+   .catch(err => console.log(err));
 });
 
 router.post("/login/test", (req, res) => {
   const { email, password } = req.body;
   user.getByEmail(email)
   .then(data => {
-    const authorized = data ? data.password === password : false;
+   // const authorized = data ? data.password === password : false;
+   return compare(password, user.password).then( authorized => {
     return res.json(authorized ? data : false)
+   })
   })
   .catch(err => console.log(err))
 });
